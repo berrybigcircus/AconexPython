0eebba9003b5be2a2b3118d705b8eabc219e0fdc
Nicole <spiritedgravity@gmail.com>
2026-08-02 19:30:29 +0100

    tagged integration tests and updated requirements.txt

---
+++ Setup/EAsetup.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+
+import requests #for making http requests
+from Setup.APIcommon import basic_auth
+
+LOBBYURL = 'https://constructionandengineering-ea.oraclecloud.com/auth/token' #endpoint
+env = "https://ea1.aconex.com"
+
+load_dotenv()
+
+clientID = os.getenv("EA_CLIENT_ID")
+clientSecret = os.getenv("EA_CLIENT_SECRET")
+
+#Request a token
+token = basic_auth(clientID, clientSecret)
+
+headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+           'Authorization': token}
+
+body = {'grant_type':'client_credentials',
+        'user_id':'1879050797',
+        'user_site':env}
+
+response = requests.post(LOBBYURL, headers=headers, params=body)
+print(str(response.status_code) + " " + response.reason)
+jsonResponse = response.json()
+bearer = 'Bearer ' + jsonResponse['access_token'] #Use the Access Token to make authorized requests to Aconex APIs on behalf of the authenticated user
+
+password = os.getenv("PASSWORD")
---
+++ Setup/UK1setup.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+
+import requests #for making http requests
+from Setup.APIcommon import basic_auth
+
+LOBBYURL = 'https://constructionandengineering.oraclecloud.com/auth/token' #endpoint
+env = "https://uk1.aconex.co.uk"
+
+load_dotenv()
+
+clientID = os.getenv('UK_CLIENT_ID')
+clientSecret = os.getenv('UK_CLIENT_SECRET')
+
+#Request a token
+token = basic_auth(clientID, clientSecret)
+
+headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+           'Authorization': token}
+
+body = {'grant_type':'client_credentials',
+        'user_id':'269118732',
+        'user_site':env}
+
+response = requests.post(LOBBYURL, headers=headers, params=body)
+print(str(response.status_code) + " " + response.reason)
+jsonResponse = response.json()
+bearer = 'Bearer ' + jsonResponse['access_token'] #Use the Access Token to make authorized requests to Aconex APIs on behalf of the authenticated user
+
